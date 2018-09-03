8c89d2f6c73fd4d5319c8e71a32dc5ea984eefbe
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 15:13:16 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exerice 1</title>
+  </head>
+  <body>
+    <form action="user.php" method="get">
+      <label for="lastname">Nom</label><input type="text" name="lastname">
+      <label for="firstname">Prénom</label><input type="text" name="firstname">
+      <input type="submit" name="button" value="Valider">
+    </form>
+  </body>
+</html>
